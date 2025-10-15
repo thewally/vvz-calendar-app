c5845f51a30196ff322635507b200287c522d38a
Arjen van der Wal <arjen.vanderwal@ns.nl>
2025-10-15 20:30:08 +0200

    Fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,129 @@
-import Image from "next/image";
+import moment from 'moment';
 
-export default function Home() {
+export default async function Home() {
+
+  const cal_id = '98a30a37d1a3e1909c7a4db5b91430c7494ef2dab8673929aa80f2305edffe13@group.calendar.google.com';
+
+
+  const current_date_time = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+  const maanden = ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"];
+  const dagen = ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"];
+
+  Date.prototype.getFullMinutes = function () {
+    if (this.getMinutes() < 10) {
+      return '0' + this.getMinutes();
+    }
+    return this.getMinutes();
+  };
+
+  Date.prototype.getFullHours = function () {
+    if (this.getHours() < 10) {
+      return '0' + this.getHours();
+    }
+    return this.getHours();
+  };
+
+  const apikey = process.env.API_KEY;
+
+  const params = new URLSearchParams();
+  params.append("key", `${apikey}`);
+  params.append("singleEvents", "True");
+  params.append("orderBy", "startTime");
+  params.append("timeMin", `${current_date_time}`);
+
+  const calendar_urls = [
+    '969ec74f52b7007ab3cfaf306e807939f0da397f543ed311a9bc4cb115710516@group.calendar.google.com',
+    '98a30a37d1a3e1909c7a4db5b91430c7494ef2dab8673929aa80f2305edffe13@group.calendar.google.com',
+    '42245e5ec6cbfa0a3edfebafe6f7ba69e6a34b319712db1499a1374172691a0a@group.calendar.google.com'
+  ];
+
+  const urls = calendar_urls.map(url =>
+    `https://www.googleapis.com/calendar/v3/calendars/${url}/events?${params}`
+  )
+
+  var events = new Array();
+  await callApis(urls)
+    .then(calendars =>
+      calendars.forEach(calendar =>
+        calendar.items.forEach(item =>
+          events.push({ 'calendarname': calendar.summary, 'item': item })
+        )
+      )
+    );
+
+  events.sort(function (a, b) {
+    return Date.parse(a.item.start.date != null ? a.item.start.date : a.item.start.dateTime) - Date.parse(b.item.start.date != null ? b.item.start.date : b.item.start.dateTime);
+  })
+
+  const getDescriptionValues = (text) => {
+    const regex = /\[([^\][]*)]/g;
+    const matches = String(text).match(regex);
+    const result = new Array();
+    if (matches) {
+      for (let i = 0; i < matches.length; ++i) {
+        const match = matches[i];
+        const withoutBrackets = match.substring(1, match.length - 1); // brackets removing
+        const splitted = withoutBrackets.split('#');
+        result.push({ 'key': splitted[0], 'value': splitted[1] });
+      }
+    }
+    return result;
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      {events.map((event) => {
+        const fullDayEvent = event.item.start.date != null && event.item.end.date != null;
+        const startDatum = fullDayEvent ? new Date(event.item.start.date) : new Date(event.item.start.dateTime);
+        const eindDatum = fullDayEvent ? new Date(event.item.end.date) : new Date(event.item.end.dateTime);
+        const descriptionValues = getDescriptionValues(event.item.description);
+        const url = getValueByKey(descriptionValues, 'url');
+        const text = getValueByKey(descriptionValues, 'text');
+        return (
+          <div key={event.item.id} className="events-wrapper">
+            <div className="events-container">
+              <div>
+                <div className="events-date">
+                  <p className="event-weekdag">{dagen[startDatum.getDay()]}</p>
+                  <p className="event-maand">{maanden[startDatum.getMonth()]}</p>
+                  <p className="event-dag">{startDatum.getDate()}</p>
+                </div>
+              </div>
+              <div className="events-body">
+                <div className="events-header">
+                  {url != undefined ? <p><a href={url}>{event.item.summary.toUpperCase()}</a></p> : <p>{event.item.summary.toUpperCase()}</p>}
+                </div>
+                <div className="events-location small">
+                  <p>{event.item.location}</p>
+                </div>
+                <div className="events-duration bold italic">
+                  {fullDayEvent ? <p>De hele dag</p> : <p>{startDatum.getFullHours()}:{startDatum.getFullMinutes()} - {eindDatum.getFullHours()}:{eindDatum.getFullMinutes()} uur</p>}
+                </div>
+                <div>
+                  <p>{text}</p>
+                </div>
+                <div className="events-location small">
+                  <p>{event.calendarname}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+      )}
     </div>
-  );
+  )
+
+  async function callApis(urls) {
+    const response = await Promise.all(urls.map(u => fetch(u)));
+    const data = await Promise.all(response.map(r => r.json()));
+    return data;
+  }
+
+  function getValueByKey(arr, key) {
+    for (var i = 0, iLen = arr.length; i < iLen; i++) {
+      if (arr[i].key == key) return arr[i].value;
+
+    }
+  }
 }
