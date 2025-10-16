c1ccb4c09b92f1bfbd526a003a3b1006df6619a3
Arjen van der Wal <arjen.vanderwal@ns.nl>
2025-10-16 08:36:37 +0200

    Add forEach

--- src/app/page.js
+++ src/app/page.js
@@ -44,8 +44,8 @@ export default async function Home() {
   var events = new Array();
   await callApis(urls)
     .then(calendars =>
-      calendars.map(calendar =>
-        calendar.items.map(item =>
+      calendars.forEach(calendar =>
+        calendar.items.forEach(item =>
           events.push({ 'calendarname': calendar.summary, 'item': item })
         )
       )
